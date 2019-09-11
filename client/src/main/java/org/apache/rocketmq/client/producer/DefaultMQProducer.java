967d2ff463cb0177ec77db4410c6548018000736
zzm <zzm@163.com>
2019-09-11 19:41:13 +0800

    发送者入口类注释

--- client/src/main/java/org/apache/rocketmq/client/producer/DefaultMQProducer.java
+++ client/src/main/java/org/apache/rocketmq/client/producer/DefaultMQProducer.java
@@ -58,25 +58,19 @@ import org.apache.rocketmq.remoting.exception.RemotingException;
  * and used among multiple threads context.
  * </p>
  */
+/**
+ * 这是应用程序使用rocketmq发送消息的入口类
+ * 先实例化DefaultMQProducer一个实例，然后调用send方法发送消息
+ * DefaultMQProducer负责配置mq的各种参数，并转发消息，消息的具体发送过程由DefaultMQProducerImpl实现
+ */
 public class DefaultMQProducer extends ClientConfig implements MQProducer {
 
     private final InternalLogger log = ClientLogger.getLog();
 
-    /**
-     * Wrapping internal implementations for virtually all methods presented in this class.
-     */
+    //rocketmq发送消息的具体内部实现
     protected final transient DefaultMQProducerImpl defaultMQProducerImpl;
 
-    /**
-     * Producer group conceptually aggregates all producer instances of exactly same role, which is particularly
-     * important when transactional messages are involved.
-     * </p>
-     *
-     * For non-transactional messages, it does not matter as long as it's unique per process.
-     * </p>
-     *
-     * See {@linktourl http://rocketmq.apache.org/docs/core-concept/} for more discussion.
-     */
+    //发送者群组
     private String producerGroup;
 
     /**
@@ -84,49 +78,34 @@ public class DefaultMQProducer extends ClientConfig implements MQProducer {
      */
     private String createTopicKey = MixAll.AUTO_CREATE_TOPIC_KEY_TOPIC;
 
-    /**
-     * Number of queues to create per default topic.
-     */
+    //一个topic创建队列数目
     private volatile int defaultTopicQueueNums = 4;
 
-    /**
-     * Timeout for sending messages.
-     */
+    //发送消息的默认超时时间
     private int sendMsgTimeout = 3000;
 
-    /**
-     * Compress message body threshold, namely, message body larger than 4k will be compressed on default.
-     */
+    //发送消息时超过阈值4K，将进行压缩
     private int compressMsgBodyOverHowmuch = 1024 * 4;
 
-    /**
-     * Maximum number of retry to perform internally before claiming sending failure in synchronous mode.
-     * </p>
-     *
-     * This may potentially cause message duplication which is up to application developers to resolve.
-     */
+    ///同步发送消息失败的默认重试次数
+    //这可能导致消息重复，需要业务保证幂等
     private int retryTimesWhenSendFailed = 2;
 
-    /**
-     * Maximum number of retry to perform internally before claiming sending failure in asynchronous mode.
-     * </p>
-     *
-     * This may potentially cause message duplication which is up to application developers to resolve.
-     */
+    ///异步发送消息失败的默认重试次数
+    //这可能导致消息重复，需要业务保证幂等
     private int retryTimesWhenSendAsyncFailed = 2;
 
-    /**
-     * Indicate whether to retry another broker on sending failure internally.
-     */
+    //当发送失败时是否尝试发送到其他broker
     private boolean retryAnotherBrokerWhenNotStoreOK = false;
 
-    /**
-     * Maximum allowed message size in bytes.
-     */
+    //消息最大的大小
     private int maxMessageSize = 1024 * 1024 * 4; // 4M
 
     /**
-     * Interface of asynchronous transfer data
+     * 异步传输数据的接口
+     * 在一个集群中可以配置一台机器专门负责消息轨迹的收集工作
+     *  该台机器上配置traceTopicEnable = true
+     *  borker启动的时候自动创建默认轨迹topic
      */
     private TraceDispatcher traceDispatcher = null;
 
@@ -169,11 +148,15 @@ public class DefaultMQProducer extends ClientConfig implements MQProducer {
         this.producerGroup = producerGroup;
         defaultMQProducerImpl = new DefaultMQProducerImpl(this, rpcHook);
         //if client open the message trace feature
+        //enableMsgTrace为true，表示开启消息轨迹跟踪
         if (enableMsgTrace) {
             try {
+                //创建一个轨迹消息发送者，一个线程池用于异步执行消息发送
                 AsyncTraceDispatcher dispatcher = new AsyncTraceDispatcher(customizedTraceTopic, rpcHook);
                 dispatcher.setHostProducer(this.defaultMQProducerImpl);
                 traceDispatcher = dispatcher;
+                //为消息轨迹注册hook,在消息发送前执行
+                //SendMessageTraceHookImpl 实现了SendMessageHook接口，在消息发送前后会被调用
                 this.defaultMQProducerImpl.registerSendMessageHook(
                     new SendMessageTraceHookImpl(traceDispatcher));
             } catch (Throwable e) {
@@ -281,10 +264,12 @@ public class DefaultMQProducer extends ClientConfig implements MQProducer {
      */
     @Override
     public void start() throws MQClientException {
+        //将namespace添加到producerGroup中
         this.setProducerGroup(withNamespace(this.producerGroup));
         this.defaultMQProducerImpl.start();
         if (null != traceDispatcher) {
             try {
+                //启动消息追踪的线程，循环发送100条消息
                 traceDispatcher.start(this.getNamesrvAddr(), this.getAccessChannel());
             } catch (MQClientException e) {
                 log.warn("trace dispatcher start failed ", e);
